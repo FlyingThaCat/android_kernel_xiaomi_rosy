baa43461248df1a77ccba4c74002a9464309f82d
FlyingThaCat <95353217+FlyingThaCat@users.noreply.github.com>
2023-04-06 23:08:37 +0700

    Update fdt.c

--- drivers/of/fdt.c
+++ drivers/of/fdt.c
@@ -1158,6 +1158,20 @@ int __init early_init_dt_scan_chosen(unsigned long node, const char *uname,
 		of_fdt_crc32 = crc32_be(~0, initial_boot_params,
 				fdt_totalsize(initial_boot_params));
 	}
+	
+	#ifdef CONFIG_CMDLINE_DROP_DANGEROUS_ANDROID_OPTIONS
+	pr_err("Replacing dangerous cmdline options...");
+	cmdline = strstr((const char *)data, "skip_initramfs");
+	if (cmdline)
+		*cmdline = '_';
+	cmdline = strstr((const char *)data, "root=");
+	if (cmdline)
+		*cmdline = '_';
+	cmdline = strstr((const char *)data, "init=");
+	if (cmdline)
+		*cmdline = '_';
+	pr_err("Command line now is: %s\n", (char*)data);
+	#endif
 
 	/* break now */
 	return 1;
